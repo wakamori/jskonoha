4e7a199315dc3b40eb4b310b95ac5d088dd410f2
masakiishii <masaki.ishii511@gmail.com>
2012-06-06 17:28:47 +0900

    update

--- src/konoha_namespace.js
+++ src/konoha_namespace.js
@@ -39,7 +39,7 @@ konoha.isalnum = function(c) {
 konoha.assert = function(cond, msg) {
 	if (!cond) {
 		var e = "Assersion!! " + msg;
-		console.log(e);
+//		console.log(e);
 		throw e;
 	}
 }
--- test/Debug/tokenize_test.js
+++ test/Debug/tokenize_test.js
@@ -9,7 +9,7 @@ tokenizeTest.prototype.ReturnCorrectTokenwords = function() {
 	var _ctx = null;
 	var tenv = new konoha.tenv_t;
 	tenv.source = "if(n < 3) {\n return 1;\n}";
-//	tenv.fmat = 4;
+	tenv.fmat = konoha.MiniKonohaTokenMatrix;
 	konoha.tokenize(_ctx, tenv);
 	expectThat(tenv.list.data, elementAre(['if', '(', 'n', '<', '3', ')', '{', 'return', '1', ';', '}']));
 
